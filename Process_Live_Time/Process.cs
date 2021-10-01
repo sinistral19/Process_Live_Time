b49d2fb342d8ab469911625f17c01d6e72dcc858
David <gubievdavid@gmail.com>
2021-10-01 13:45:25 +0300

    Добавьте файлы проекта.

---
+++ Process_Live_Time/Process.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Process_Live_Time
+{
+    class Process
+    {
+        public static string Process_name { get; set; }
+        public static TimeSpan? Time_live { get; set; }
+        public static int? Time_search { get; set; }
+
+
+
+
+        
+
+
+       public  static void Search_Process()
+         {
+            Console.WriteLine($"Начало поиска {Process_name}");
+            Boolean prod_poisk = true;
+
+             while (prod_poisk == true)
+             {
+                 System.Diagnostics.Process[] arry = System.Diagnostics.Process.GetProcesses();
+
+                 foreach (System.Diagnostics.Process pr in arry)
+                 {
+                     if (pr.ProcessName == Process_name)
+                     {
+
+                         TimeSpan time = DateTime.Now - pr.StartTime;
+
+                         //   pr.Close();
+
+                         if (time > Time_live)
+                         {
+                             pr.Kill();
+
+                            Thread.Sleep(2000);
+
+                            if (System.Diagnostics.Process.GetProcessesByName(Process_name).Length> 0)
+                            {
+                                Console.WriteLine($"Один процесс {Process_name} завершен.В системе есть еще процессы с именем {Process_name}. Продолжить поиск?(да/нет)");
+                                string con = Console.ReadLine();
+                                if (con == "да")
+                                {
+                                    goto m1;
+                                }
+                                else if (con == "нет")
+                                {
+                                    prod_poisk = false;
+                                    break;
+                                }
+                            }
+                            else 
+                            {
+                                prod_poisk = false;
+                                break;
+                            }
+                             
+
+
+
+
+
+                        
+
+
+                             //goto m1;
+                         }
+
+                     }
+
+
+                     //   m1:;
+                 }
+           
+
+
+
+
+
+
+                if (System.Diagnostics.Process.GetProcessesByName(Process_name).Length == 0)
+                {
+                    Console.WriteLine($"В системе нет  процессов с именем {Process_name}. Продолжить поиск?(да/нет)");
+                    string con = Console.ReadLine();
+                    if (con == "да")
+                    {
+                        goto m1;
+                    }
+                    else if (con == "нет")
+                    {
+                        prod_poisk = false;
+                        
+                    }
+                }
+
+
+            m1:;
+                if (prod_poisk == true)
+                {
+                    //Task.Delay((int)Time_search);
+
+                    Thread.Sleep((int)Time_search);
+                }
+
+
+
+            }
+         //  res= "Проложение завершило поиск.";
+            Console.WriteLine("Проложение завершило поиск.");
+        }
+
+     /*   public static async void Poisk()
+            {
+            string res = "";
+                Console.WriteLine($"Начало поиска {Process_name}");
+                await Task.Run(() => Search_Process());
+         
+
+        }*/
+        }
+    } 
---
+++ Process_Live_Time/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Process_Live_Time;
+namespace Process_Live_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+              if (args[0] != "arg1")
+              {
+                Process.Process_name = args[0];
+                Process.Time_live = new TimeSpan(0, Convert.ToInt32(args[1]), 0);
+                Process.Time_search = Convert.ToInt32(args[2])*(60000);
+              }
+
+
+           
+
+            if (args[0]=="arg1"){
+
+                Process.Process_name = "notepad";
+                Process.Time_live = new TimeSpan(0, 1, 0);
+                Process.Time_search = (6000);
+            }
+
+        Process.Search_Process();
+            /*
+                        Console.WriteLine("Для завершения работы нажмите esc.");
+
+                        if (Console.ReadKey().Key == ConsoleKey.Escape)
+                        {
+                            Environment.Exit(0);
+                        }
+                        */
+
+
+            Console.WriteLine();
+
+        }
+    }
+}
